c743d18bc6b57c5ecd6c037e10124a6a6619cee5
manhowong <92902645+manhowong@users.noreply.github.com>
2026-04-09 22:23:15 +0100

    Simplify instructions and legend.

--- dashboard/scripts/ui/ui-text.js
+++ dashboard/scripts/ui/ui-text.js
@@ -18,53 +18,43 @@ export const INSUFFICIENT_DATA_TEXT = 'Insufficient data.';
 export const NO_RELEVANT_NODES_TEXT = 'No topics found with current settings.';
 export const INSTRUCTIONS_HTML = `
   <div id="instructions">
-    <span><i>Desktop</i></span>
+
+    <span><i>Go down a level</i></span>
     <ul class="noBullet-list">
-      <li><b>Click</b> a node to go down a level.</li>
-      <li><b>Double-click on empty space</b> to go up a level.</li>
+      <li>Desktop: <b>Click</b> on a node.</li>
+      <li>Mobile: <b>Tap</b> a node <b>twice</b>.</li>
     </ul>
 
-    <span><i>Mobile</i></span>
+    <span><i>Go up a level</i></span>
     <ul class="noBullet-list">
-      <li><b>Tap</b> a node <b>twice</b> to go down a level.</li>
-      <li><b>Long-press anywhere</b> to go up a level.</li>
+      <li>Desktop: <b>Double-click on empty space</b>.</li>
+      <li>Mobile: <b>Long-press anywhere</b>.</li>
     </ul>
 
     <span>
-        You can also click/tap on a node in <b>this panel</b>.<br />
+        You can also navigate in <b>this panel</b>.<br />
     </span>
 
-    <span>
-        To <b>search</b>, press "Find Topics" or 
-        <span class="mockKbd">Ctrl</span> + <span class="mockKbd">K</span>
-    </span>
-
-    <span>
-      To see <b>trends</b>, set a range ≥ 2 months in 
-      <span class="mockToggle">
-        <span class="mock-hamburger-icon">CONTROL</span>
-      </span>
-    </span>
   </div>`;
 
 export const LEGEND_HTML = `
   <div id="legend-block">
 
-    <p><b>Article count</b>: <br/> Shown after node name</p>
-    <p><b>Node size</b>: <br/> Scales with article count (normalized)</p>
-    <p><b>Link width</b>: <br/> Scales with relevance (Dice-Sørensen Coefficient)</p>
+    <p><b>Article count</b> <br/> Shown after node name</p>
+    <p><b>Node size</b> <br/> Scales with article count (normalized)</p>
+    <p><b>Link width</b> <br/> Scales with relevance (Dice-Sørensen Coefficient)</p>
     
     <p>
-        <b>Node color</b>: <br/>
-        Direction of <i>Hotness Score</i> (Period-to-Period change in share of total articles):
+        <b>Node color</b> <br/>
+        Direction of <i>Hotness Score</i> (Period-to-Period change* in share of total articles):
         <div id="legend-item">
             <span id="legend-dot" style="background:var(--trend-up)"></span>
-            Heating up ( > +20% )
+            Heating up ( >= +20% )
         </div>
 
         <div id="legend-item">
             <span id="legend-dot" style="background:var(--trend-down)"></span>
-            Cooling off  ( < &minus;20% )
+            Cooling off  ( <= &minus;20% )
         </div>
         
         <div id="legend-item">
@@ -72,14 +62,19 @@ export const LEGEND_HTML = `
             No trends detected
         </div>
 
-        <span id="legend-note">
+        <div id="legend-note">
             *Hotness score requires <b>TWO</b> time points.
-        </span>
+        </div>
     </p>
 
     <p>
-        <b>Area vs. Topic nodes</b>: <br/>
-        An area node is an aggregate of its child topic nodes. Links between area nodes are aggregates of links between topic nodes.
+        <b>Area vs. Topic nodes</b> <br/>
+        An area node is an aggregate* of its child topic nodes. Links between area nodes are aggregates* of links between topic nodes.
+
+        <div id="legend-note">
+            *Filtering data at Topic level <b>affects</b> Area nodes.
+        </div>
+    
     </p>
 
   </div>`;
